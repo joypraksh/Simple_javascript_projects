c5fed9a970513a0884285c0bfb07db7f926e46fb
Joypraksh Sarma <khanjansarma280@gmail.com>
2023-10-26 09:13:56 -0700

    add new js project which id shopping card

--- to_do_app/script.js
+++ to_do_app/script.js
@@ -121,9 +121,8 @@ function displayFilteredTodos(filteredTodos) {
     card_text.innerHTML = html;
 }
 
-
-
 $(window).load(function () {
     $(".loader").delay(1000).fadeOut("slow");
     $("#overlayer").delay(1000).fadeOut("slow");
 });
+  
